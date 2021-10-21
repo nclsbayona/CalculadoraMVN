8f77feff86eaa7e4e414b42a62779ae995c58dc3
nclsbayona <nbayonaord@gmail.com>
2021-10-21 15:16:21 -0500

    Hola

---
+++ demo/src/main/java/businessLogic/Calculadora.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogic;
+
+/**
+ *
+ * @author Anabel
+ */
+public class Calculadora {
+    
+    public static float add(float a, float b)
+    {
+        return a+b;
+    }
+    
+    public static float substract(float a, float b)
+    {
+        return a-b;
+    }
+    
+    public static float multiply(float a, float b)
+    {
+        return a*b;
+    }
+    
+    public static float div(float a, float b)
+    {
+        return a/b;
+    }
+}
