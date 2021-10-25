e6e802ad63b04a54fd03bd15dfc25fe9f557d364
David Enrique Palacios Garcia <paladavid@hotmail.com>
2021-10-25 16:43:06 -0500

    New methods added

--- demo/src/main/java/businessLogic/Calculadora.java
+++ demo/src/main/java/businessLogic/Calculadora.java
@@ -21,4 +21,37 @@ public class Calculadora {
     {
         return a/b;
     }
+
+    public static float mod(float a, float b)
+    {
+        return a%b;
+    }
+
+    public static float sqrt(float a)
+    {
+        return (float)Math.sqrt(a);
+    }
+    public static float signo(float a)
+    {
+        return -a;
+    }
+    public static float nfactorial(float a)
+    {
+        float factorial = 1;
+        for(int i = 1; i <= a; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static float log(float a)
+    {
+        return (float)Math.log(a);
+    }
+
+    public static float tenPow(float a)
+    {
+        return (float)10 * a;
+    }
 }
